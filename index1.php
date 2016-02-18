35f47e96f7baa5b3507e6dfe0aa2e0dcba165800
casconalio <compto51@msu.edu>
2016-02-18 13:36:43 -0500

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Team Amazon - Twitch</title>
+    <link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<h1>CSE 498 Capstone - Team Amazon - Twitch.tv
+	<p><a href="twitch_mod/twitch.php">Twitch!</a></p>
+	<p><a href="twitch_mod/chatpage.html">Chat html</a></p>
+	<p><a href="twitch_mod/chat.php">Twitch chat 1.0</a></p>
+
+	<p><a href="https://github.com/casconalio/twitch.git">Github</a></p>
+	<p><a href="misc">Extras</a></p>
+</body>
+</html>
+
