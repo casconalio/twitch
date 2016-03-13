bf5e6101a39acad6b2954da6fb190b943e98dfba
Michael Chen <chenxi28@msu.edu>
2016-03-13 16:17:08 -0400

    updated read

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 ### twitch
 Capstone Twitch.tv project
 
-> Mha homeboi Ca$h made dis
-> respect him
+
+Advertisements Php
+> Advertisement keywords sending and receiving scripts
+
+Alpha
+> Alpha presentation backup code
+
+MikeTheThug
+> Twitch Chat section written in php
+
+cash
+> Twitch Chat UI files
+
+feb19/feb20
+> Alpha presentation
+
+misc
+> Some old code
+
+twitch_mod
+> Old website
+
+www
+> Team Amazon homepage
---
+++ www/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Team Amazon</title>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+
+    <!-- Bootstrap theme -->
+    <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link rel="shortcut icon" type="image/x-icon" href="icon/favicon.ico">
+  </head>
+  <body>
+    <div id="pollMQ"></div>
+
+<!-- Fixed navbar -->
+<nav class="flip-nav">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <nav class="pull">
+                    <ul class="top-nav">
+
+                        <li><a href="http://35.9.22.102/twitch/fasterChat.php"> Chat 1.0 <span class="indicator"><i class="fa fa-angle-right"></i></span></a></li>
+                        <li><a href="http://35.9.22.102/Alpha/twitch/twitch.php"> Alpha <span class="indicator"><i class="fa fa-angle-right"></i></span></a></li>
+                        <li><a href="#"> Beta (in progress) <span class="indicator"><i class="fa fa-angle-right"></i></span></a></li>
+                        <li><a href="https://github.com/casconalio/twitch.git"> GitHub <span class="indicator"><i class="fa fa-angle-right"></i></span></a></li>
+                        <li><a href="http://35.9.22.102/misc"> Extras <span class="indicator"><i class="fa fa-angle-right"></i></span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+<a id="nav-toggle" class="nav_slide_button" href="#"><span></span></a>
+
+<div id="top-hero">
+    <div class="container" data-stellar-ratio="0.5">
+        <div class="row text-center">
+            <div class="col-sm-8 col-sm-offset-2 hero-title waypoint-1">
+                <h1 class="xlarge kern-loose title">
+                    TEAM AMAZON
+                </h1>
+                <h2 class="h4 sub-title">
+                    Cash / Chaz / Max / Michael / Po-An
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.flexslider.js"></script>
+<script src="assets/js/jquery.stellar.js"></script>
+<script src="assets/js/scripts.js"></script>
+  </body>
+</html>
