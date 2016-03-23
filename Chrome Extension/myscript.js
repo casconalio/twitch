97739842a77392715461ae3e2a3bc94990d576be
casconalio <compto51@msu.edu>
2016-03-23 13:47:48 -0400

    style updates, changes channel on our page

--- Chrome Extension/myscript.js
+++ Chrome Extension/myscript.js
@@ -1,3 +1,5 @@
+//adding "Twitch 2.0" to all twitch.tv streams beneath video player in-line with channel stats.
+
 function inject() {
       if(document.getElementsByClassName('channel-stats')[0]){
             document.getElementsByClassName('channel-stats')[0].appendChild(Link);
@@ -5,35 +7,26 @@ function inject() {
       }
 }
 
+function setChannel() {
+      var channel = window.location.href;
+      channel = channel.substr(22);
+      return channel;
+}
+
 if(document.getElementsByClassName("channel_new columns")[0]) {
+      var channelName = setChannel();
+
       var Link       = document.createElement('a');
-      Link.href      = 'http://35.9.22.102/beta/twitch/twitch.php';
+      Link.href      = 'http://35.9.22.102/beta/twitch/twitch.php?c=' + channelName;
       Link.target    = '_blank';
-      Link.className = 'channel-name';
+      Link.className = 'action button js-sub-button primary subscribe-button';
       Link.setAttribute('style', 'float:left;');
 
       var Container = document.createElement('span');
       Container.setAttribute('id','injected');
-      Container.setAttribute('style', 'padding-top:1px;font-weight:200%;');
-      Container.innerHTML = 'Twitch 2.0';
+      Container.className = 'subscribe-text';
+      Container.innerHTML = 'Twitch 2.0 &nbsp';
       
       Link.appendChild(Container);
       var intervalId = setInterval(inject, 500);
 }
-/*
-if(document.getElementsByClassName("ad")[0]){
-      alert("we're in");
-      var Link       = document.createElement("a");
-      Link.href      = 'http://35.9.22.102/beta/twitch/twitch.php';
-      Link.target    = '_blank';
-      Link.className = 'channel-stats';
-      Link.setAttribute('style', 'float:left;');
-
-      var Container = document.createElement('span');
-      Container.setAttribute('id', 'twitch-improvements-link');
-      Container.className = 'twitch-improvements';
-      Container.innerHTML = 'Improved Twitch';
-
-      Link.appendChild(Container);
-      document.getElementById('topCmts').appendChild(Link);
-}*/
